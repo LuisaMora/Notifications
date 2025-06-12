56ba6f086c67d8aec5e9462fbd768b9d058f5b56
Luisa Mora <luisamora895@gmail.com>
2025-06-12 13:11:56 -0400

    ADD SpringBootDataRest

--- notification-persistence/src/main/java/com/dharbor/bootcamp/notificationpersistence/domain/Notification.java
+++ notification-persistence/src/main/java/com/dharbor/bootcamp/notificationpersistence/domain/Notification.java
@@ -1,8 +1,8 @@
 package com.dharbor.bootcamp.notificationpersistence.domain;
 
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.DBRef;
 import org.springframework.data.mongodb.core.mapping.Document;
 
